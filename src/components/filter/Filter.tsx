c6412c74946d6b3784a5dd6487c05d00e78fceb1
David Ursueguía <david.ursueguia@gmail.com>
2024-10-31 20:41:05 +0100

    feat: added filter input and podcasts counter

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,10 +1,25 @@
 import {Component} from 'react';
+import {Chip, TextField} from "@mui/material";
 
 class Filter extends Component {
     render() {
         return (
-            <div style={{float: "right"}}>
-                <span>Filter</span>
+            <div style={{
+                float: "right",
+                marginTop: "10px",
+                marginRight: "10px",
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: "10px"
+            }}>
+                <Chip color="info" label={'100'}/>
+                <TextField
+                    label={'Filter podcasts...'}
+                    variant={'outlined'}
+                    onClick={() => {
+                        console.log('TODO');
+                    }}/>
             </div>
         );
     }
