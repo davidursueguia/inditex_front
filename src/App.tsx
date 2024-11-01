c7cf0c2d8096ccc3f1bf5e1a7729f408a2b66b1c
David Ursueguía <david.ursueguia@gmail.com>
2024-11-01 22:07:57 +0100

    feat: add browser routes with its routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import './App.css'
+import './App.css';
 import PodcastList from "./components/podcast-list/PodcastList.tsx";
 import Header from "./components/header/Header.tsx";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import PodcastDetail from "./components/podcast-detail/PodcastDetail.tsx";
 
 function App() {
-
     return (
-        <>
-            <BrowserRouter>
-                <Header/>
-                <PodcastList/>
-            </BrowserRouter>
-        </>
-    )
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<PodcastList/>}/>
+                <Route path="/podcast/:id" element={<PodcastDetail/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
