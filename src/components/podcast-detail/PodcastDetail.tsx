10f9da8a1ddd6b5267531c56a529c59cb0f7da85
David Ursueguía <david.ursueguia@gmail.com>
2024-11-04 17:42:42 +0100

    feat: refactor styles

--- src/components/podcast-detail/PodcastDetail.tsx
+++ src/components/podcast-detail/PodcastDetail.tsx
@@ -9,7 +9,15 @@ import {
 } from "@mui/material";
 import {formatDate, formatMillisecondsToMinutesSeconds} from "../../utils/dateTimeUtils.ts";
 import {usePodcastData} from "../../hooks/usePodcastData.ts";
-import {StyledTableCell, StyledTableRow} from "./styles.ts";
+import {
+    Container,
+    EpisodeLink,
+    EpisodesCard,
+    EpisodesContainer,
+    StyledTableCell,
+    StyledTableRow,
+    Title
+} from "./styles.ts";
 import {Link, useLocation} from "react-router-dom";
 import PodcastInfoCard from "../podcast-info-card/PodcastInfoCard.tsx";
 
@@ -18,19 +26,17 @@ const PodcastDetail = () => {
     const {selectedPodcast, podcastEpisodes, isLoading} = usePodcastData();
 
     return (
-        <div style={{display: 'grid', gridTemplateColumns: '1fr 3fr', gap: '20px', padding: '20px'}}>
+        <Container>
             <PodcastInfoCard selectedPodcast={selectedPodcast} isLoading={isLoading} />
-            <div style={{display: 'grid', alignContent: 'baseline', gap: '20px'}}>
-                <Card style={{borderRadius: '8px'}}>
-                    <h3 style={{marginLeft: '10px', textAlign: 'left', fontSize: '1.2em', color: '#333'}}>
-                        {'Episodes: ' + (podcastEpisodes?.length || 0)}
-                    </h3>
-                </Card>
+            <EpisodesContainer>
+                <EpisodesCard>
+                    <Title>{'Episodes: ' + (podcastEpisodes?.length || 0)}</Title>
+                </EpisodesCard>
                 <TableContainer component={Paper}>
                     <Table>
                         <TableHead>
                             <StyledTableRow>
-                                <StyledTableCell align={'left'}>Title</StyledTableCell>
+                                <StyledTableCell align="left">Title</StyledTableCell>
                                 <StyledTableCell align="right">Date</StyledTableCell>
                                 <StyledTableCell align="right">Duration</StyledTableCell>
                             </StyledTableRow>
@@ -40,36 +46,36 @@ const PodcastDetail = () => {
                                 Array.from(new Array(5)).map((_, index) => (
                                     <StyledTableRow key={index}>
                                         <StyledTableCell>
-                                            <Skeleton/>
+                                            <Skeleton />
                                         </StyledTableCell>
                                         <StyledTableCell align="right">
-                                            <Skeleton/>
+                                            <Skeleton />
                                         </StyledTableCell>
                                         <StyledTableCell align="right">
-                                            <Skeleton/>
+                                            <Skeleton />
                                         </StyledTableCell>
                                     </StyledTableRow>
                                 ))
                             ) : (
                                 podcastEpisodes && podcastEpisodes.map((row) => (
-                                    <StyledTableRow
-                                        key={row.trackId}
-                                    >
+                                    <StyledTableRow key={row.trackId}>
                                         <StyledTableCell component="th" scope="row">
-                                            <Link
-                                                to={`${location.pathname}/episode/${row.trackId}`}>{row.trackName}</Link>
+                                            <EpisodeLink to={`${location.pathname}/episode/${row.trackId}`}>
+                                                {row.trackName}
+                                            </EpisodeLink>
                                         </StyledTableCell>
                                         <StyledTableCell align="right">{formatDate(row.releaseDate)}</StyledTableCell>
-                                        <StyledTableCell
-                                            align="right">{formatMillisecondsToMinutesSeconds(row.trackTimeMillis)}</StyledTableCell>
+                                        <StyledTableCell align="right">
+                                            {formatMillisecondsToMinutesSeconds(row.trackTimeMillis)}
+                                        </StyledTableCell>
                                     </StyledTableRow>
                                 ))
                             )}
                         </TableBody>
                     </Table>
                 </TableContainer>
-            </div>
-        </div>
+            </EpisodesContainer>
+        </Container>
     );
 };
 
--- src/components/podcast-detail/styles.ts
+++ src/components/podcast-detail/styles.ts
@@ -1,4 +1,6 @@
-import {styled, TableCell, tableCellClasses, TableRow} from "@mui/material";
+import styled from '@mui/material/styles/styled';
+import {Card, TableCell, tableCellClasses, TableRow} from "@mui/material";
+import {Link} from "react-router-dom";
 
 export const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -21,3 +23,35 @@ export const StyledTableRow = styled(TableRow)(({ theme }) => ({
         backgroundColor: theme.palette.action.selected, // Cambia este color según tu preferencia
     },
 }));
+
+export const Container = styled('div')`
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    gap: 20px;
+    padding: 20px;
+`;
+
+export const EpisodesContainer = styled('div')`
+    display: grid;
+    align-content: baseline;
+    gap: 20px;
+`;
+
+export const EpisodesCard = styled(Card)`
+    border-radius: 8px;
+`;
+
+export const Title = styled('h3')`
+    margin-left: 10px;
+    text-align: left;
+    font-size: 1.2em;
+    color: #333;
+`;
+
+export const EpisodeLink = styled(Link)`
+    text-decoration: none;
+    color: inherit;
+    &:hover {
+        text-decoration: underline;
+    }
+`;
